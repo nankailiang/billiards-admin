37c8ac7b09cd9ca18ff1ace9a9327f08cb664afc
nankailiang <1003857254@qq.com>
2020-11-03 22:39:30 +0800

    完成顶部导航栏和侧边导航栏布局

---
+++ src/views/layout/components/aside.vue
@@ -0,0 +1,62 @@
+<template>
+    <el-menu
+      class="nav-menu"
+      default-active="/"
+      background-color="#304156"
+      text-color="#BFCBCF"
+      active-text-color="#409EFF"
+      router
+      :collapse="isCollapse"
+    >
+      <el-menu-item index="/">
+        <i class="iconfont icon-taiqiu"></i>
+        <span slot="title">球桌管理</span>
+      </el-menu-item>
+      <el-menu-item index="member">
+        <i class="iconfont icon-huiyuanguanli"></i>
+        <span slot="title">会员管理</span>
+      </el-menu-item>
+      <el-menu-item index="user">
+        <i class="iconfont icon-gerenzhongxin"></i>
+        <span slot="title">角色管理</span>
+      </el-menu-item>
+      <el-menu-item index="role">
+        <i class="iconfont icon-key"></i>
+        <span slot="title">权限管理</span>
+      </el-menu-item>
+      <el-menu-item index="analyse">
+        <i class="iconfont icon-fenxi"></i>
+        <span slot="title">数据分析</span>
+      </el-menu-item>
+    </el-menu>
+</template>
+
+<script>
+export default {
+  name: 'AppAside',
+  components: {},
+  props: ['is-collapse'],
+  data () {
+    return {
+      // isCollapse: true
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  mothods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.nav-menu {
+    .iconfont {
+        margin-right: 8px;
+    }
+    span {
+        margin-right: 80px;
+    }
+}
+</style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,21 +1,65 @@
 <template>
-    <div class="layout-container">
-        <div>顶部路由</div>
-
-        <div>侧边导航栏</div>
-
-        <!-- 子路由出口 -->
-        <router-view />
-    </div>
+    <el-container class="layout-container">
+        <!-- 侧边导航栏 -->
+        <el-aside
+            class="aside"
+            width="auto"
+        >
+            <app-aside
+                class="aside-menu"
+                :is-collapse="isCollapse"
+            />
+        </el-aside>
+        <el-container>
+            <!-- 顶部导航栏 -->
+            <el-header class="header">
+                <div>
+                    <i
+                        :class="{
+                            'el-icon-s-fold': isCollapse,
+                            'el-icon-s-unfold': !isCollapse
+                        }"
+                        @click="isCollapse = !isCollapse"
+                    ></i>
+                    <span>临沂大学台球厅管理系统</span>
+                </div>
+                <el-dropdown>
+                    <div class="avatar-wrap">
+                        <img class="avatar" src="./avatar.jpg">
+                        <span>用户昵称</span>
+                        <i class="el-icon-arrow-down el-icon--right"></i>
+                    </div>
+                    <!-- <span class="el-dropdown-link">
+                        下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
+                    </span> -->
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item>设置</el-dropdown-item>
+                        <el-dropdown-item>退出</el-dropdown-item>
+                        <el-dropdown-item>保留</el-dropdown-item>
+                    </el-dropdown-menu>
+                </el-dropdown>
+            </el-header>
+            <el-main class="main">
+                <!-- 子路由出口 -->
+                <router-view />
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
+import AppAside from './components/aside'
 export default {
   name: 'LayoutIndex',
-  components: {},
+  components: {
+    AppAside
+  },
   props: {},
   data () {
-    return {}
+    return {
+      user: {},
+      isCollapse: false // 侧边菜单栏的展开状态
+    }
   },
   computed: {},
   watch: {},
@@ -26,5 +70,38 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.layout-container {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    .aside {
+        .aside-menu {
+            height: 100%;
+        }
+    }
+    .header {
+        background-color: #fff;
+        height: 60px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        font-size: 15px;
+        border-bottom: 1px solid #ccc;
+        .avatar-wrap {
+            display: flex;
+            align-items: center;
+            .avatar {
+                width: 35px;
+                height: 35px;
+                border-radius: 20%;
+                margin-right: 10px;
+            }
+        }
+    }
+    .main {
+        background-color: #FAFAFA;
+    }
+}
 </style>
